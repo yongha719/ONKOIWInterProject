bd9bb6298637ab3c443aacf15dfb13d25c567720
kim-jeong-min-1 <kjmgi0671@naver.com>
2022-01-24 19:14:27 +0900

    4(2022-01-24)
    연속으로 피깎이지 않게 1초 쿨타임 만듬

--- WinterProject/Assets/JUNGMIN/Scirpt/BackGround.cs
+++ WinterProject/Assets/JUNGMIN/Scirpt/BackGround.cs
@@ -5,13 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class BackGround : MonoBehaviour
 {
-    bool isOn = false;
+ 
     bool isGameClear = false;
+    bool isClickCoolTime = true;
     private int heartCount = 2;
     public Transform heartTransform;
     [SerializeField] List<GameObject> heart = new List<GameObject>();
-
-    Coroutine heartCorutine;
+ 
     // Start is called before the first frame update
     void Start()
     {
@@ -19,23 +19,8 @@ public class BackGround : MonoBehaviour
         {
             heart.Add(hT.gameObject);
         }
-
-        //heartCorutine = StartCoroutine(HeartLoop());
     }
-    //IEnumerator HeartLoop()
-    //{
-    //    while (heartCount > 0)
-    //    {
-    //        if (Input.GetMouseButtonDown(0) && isOn == true)
-    //        {
-    //            heart[heartCount].SetActive(false);
-    //            heartCount--;
-    //            yield return new WaitForSeconds(1);
-    //        }
-    //    }
-    //}
-
-    // Update is called once per frame
+    
     void Update()
     {
 
@@ -51,21 +36,22 @@ public class BackGround : MonoBehaviour
     }
 
     void OnMouseOver()
-    {
-        isOn = true;
-        if (isOn == true && isGameClear == false)
+    {      
+        if (isGameClear == false && isClickCoolTime == true)
         {
             if (Input.GetMouseButtonDown(0))
             {
                 heart[heartCount].SetActive(false);
                 heartCount--;
+                isClickCoolTime = false;
+                Invoke("CoolTime", 1f);
             }
-        }
+        }       
     }
 
-    void OnMouseExit()
+    void CoolTime()
     {
-        isOn = false;
+        isClickCoolTime = true;
     }
 
 }
