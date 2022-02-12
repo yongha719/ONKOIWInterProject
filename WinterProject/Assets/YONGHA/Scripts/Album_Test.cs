225c4591dd87434727195e061c70148609fb90a9
qkrgustn <sdh210212@sdh.hs.kr>
2022-02-12 21:30:32 +0900

    앨범 양세화 추가 & 대화하기 UI 추가

--- WinterProject/Assets/YONGHA/Scripts/Album_Test.cs
+++ WinterProject/Assets/YONGHA/Scripts/Album_Test.cs
@@ -6,10 +6,11 @@ public class Album_Test : MonoBehaviour
 {
     [SerializeField] private GameObject ChaeAhHappyAlbum, SeHwaHappyAlbum, GaYoonHappyAlbum;
     [SerializeField] private GameObject ChaeAhNormal, SeHwaNormal, GaYoonNormal;
-    [SerializeField] private GameObject ChaeAhHappy1, ChaeAhHappy2, ChaeAhHappy3;
+    [SerializeField] private GameObject ChaeAhHappy1, ChaeAhHappy2, ChaeAhHappy3, SeHwaHappy1, SeHwaHappy2, SeHwaHappy3;
     [SerializeField] private GameObject ChaeAhBtn, SeHwaBtn, GaYoonBtn;
 
     int ChaeAhCheak;
+    int SeHwaCheak;
     private void Awake()
     {
         
@@ -18,16 +19,20 @@ public class Album_Test : MonoBehaviour
     public void NormalOn()
     {
         ChaeAhNormal.SetActive(true);
+        SeHwaNormal.SetActive(true);
     }
 
     public void HappyOn()
     {
         ChaeAhHappyAlbum.SetActive(false);
+        SeHwaHappyAlbum.SetActive(false);
         ChaeAhHappy1.SetActive(true);
+        SeHwaHappy1.SetActive(true);
         ChaeAhBtn.SetActive(true);
+        SeHwaBtn.SetActive(true);
     }
 
-    public void HappyClick()
+    public void HappyClickChaeAh()
     {
         if(ChaeAhCheak == 0)
         {
@@ -48,5 +53,26 @@ public class Album_Test : MonoBehaviour
             ChaeAhCheak = 0;
         }
     }
-
+    public void HappyCheakSeHwa()
+    {
+        if (SeHwaCheak == 0)
+        {
+            SeHwaHappy1.SetActive(false);
+            SeHwaHappy2.SetActive(true);
+            SeHwaCheak++;
+        }
+        else if (SeHwaCheak == 1)
+        {
+            SeHwaHappy2.SetActive(false);
+            SeHwaHappy3.SetActive(true);
+            SeHwaCheak++;
+        }
+        else if (SeHwaCheak == 2)
+        {
+            SeHwaHappy3.SetActive(false);
+            SeHwaHappy1.SetActive(true);
+            SeHwaCheak = 0;
+        }
+    }
 }
+
