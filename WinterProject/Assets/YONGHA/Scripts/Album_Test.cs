3c14074b3fe6e6ae12bec21ecc08334b6436b523
qkrgustn <sdh210212@sdh.hs.kr>
2022-02-16 20:48:12 +0900

    clone씬에서 앨범 완료
    앨범 스크립트 보면 주석처리로 설명이 되어있는 부분이 노말/해피 엔딩 활성화 시켜주는 부분 엔딩 보면 저 코드 실행시켜주면 됨

--- WinterProject/Assets/YONGHA/Scripts/Album_Test.cs
+++ WinterProject/Assets/YONGHA/Scripts/Album_Test.cs
@@ -6,47 +6,38 @@ public class Album_Test : MonoBehaviour
 {
     [SerializeField] private GameObject ChaeAhHappyAlbum, SeHwaHappyAlbum, GaYoonHappyAlbum;
     [SerializeField] private GameObject ChaeAhNormal, SeHwaNormal, GaYoonNormal;
-    [SerializeField] private GameObject ChaeAhHappy1, ChaeAhHappy2, ChaeAhHappy3, SeHwaHappy1, SeHwaHappy2, SeHwaHappy3;
+    [SerializeField] private GameObject ChaeAhHappy1, ChaeAhHappy2, ChaeAhHappy3, SeHwaHappy1, SeHwaHappy2, SeHwaHappy3, GayoonHappy1, GayoonHappy2, GayoonHappy3;
     [SerializeField] private GameObject ChaeAhBtn, SeHwaBtn, GaYoonBtn;
 
-    int ChaeAhCheak;
-    int SeHwaCheak;
-    private void Awake()
+    int ChaeAhCheak, SeHwaCheak, GayoonCheak;
+
+    public void NormalOn() //노말 엔딩 해금 됐을 시 (물론 캐릭터마다 따로 해줘야함)
     {
-        
+        ChaeAhNormal.SetActive(true);SeHwaNormal.SetActive(true);GaYoonNormal.SetActive(true);
     }
 
-    public void NormalOn()
+    public void HappyOn() //해피 엔딩 해금 됐을 시 (물론 캐릭터마다 따로 해줘야함)
     {
-        ChaeAhNormal.SetActive(true);
-        SeHwaNormal.SetActive(true);
-    }
-
-    public void HappyOn()
-    {
-        ChaeAhHappyAlbum.SetActive(false);
-        SeHwaHappyAlbum.SetActive(false);
-        ChaeAhHappy1.SetActive(true);
-        SeHwaHappy1.SetActive(true);
-        ChaeAhBtn.SetActive(true);
-        SeHwaBtn.SetActive(true);
+        ChaeAhHappyAlbum.SetActive(false);SeHwaHappyAlbum.SetActive(false);GaYoonHappyAlbum.SetActive(false);
+        ChaeAhHappy1.SetActive(true);SeHwaHappy1.SetActive(true);GayoonHappy1.SetActive(true);
+        ChaeAhBtn.SetActive(true);SeHwaBtn.SetActive(true);GaYoonBtn.SetActive(true);
     }
 
     public void HappyClickChaeAh()
     {
-        if(ChaeAhCheak == 0)
+        if (ChaeAhCheak == 0)
         {
             ChaeAhHappy1.SetActive(false);
             ChaeAhHappy2.SetActive(true);
             ChaeAhCheak++;
         }
-        else if(ChaeAhCheak == 1)
+        else if (ChaeAhCheak == 1)
         {
             ChaeAhHappy2.SetActive(false);
             ChaeAhHappy3.SetActive(true);
             ChaeAhCheak++;
         }
-        else if(ChaeAhCheak == 2)
+        else if (ChaeAhCheak == 2)
         {
             ChaeAhHappy3.SetActive(false);
             ChaeAhHappy1.SetActive(true);
@@ -72,6 +63,27 @@ public class Album_Test : MonoBehaviour
             SeHwaHappy3.SetActive(false);
             SeHwaHappy1.SetActive(true);
             SeHwaCheak = 0;
+        }
+    }
+    public void HappyCheakGayoon()
+    {
+        if (GayoonCheak == 0)
+        {
+            GayoonHappy1.SetActive(false);
+            GayoonHappy2.SetActive(true);
+            GayoonCheak++;
+        }
+        else if (GayoonCheak == 1)
+        {
+            GayoonHappy2.SetActive(false);
+            GayoonHappy3.SetActive(true);
+            GayoonCheak++;
+        }
+        else if (GayoonCheak == 2)
+        {
+            GayoonHappy3.SetActive(false);
+            GayoonHappy1.SetActive(true);
+            GayoonCheak = 0;
         }
     }
 }
