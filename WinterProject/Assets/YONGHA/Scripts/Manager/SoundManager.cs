f2aa6ee8bf5bf81d8767afe5252bd21e4e7bdc5d
qkrgustn <sdh210212@sdh.hs.kr>
2022-02-16 21:51:38 +0900

    브금 관련 기본 셋팅

---
+++ WinterProject/Assets/YONGHA/Scripts/Manager/SoundManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class SoundManager : Singleton<SoundManager>
+{
+    //0-스토리 1-노말엔딩 2-눈싸움 3-대화 4-메인,해피 5-비디오게임 6-찾기게임
+    public AudioSource[] BGM;
+
+    private void Start()
+    {
+        BGM[4].Play();
+    }
+}
